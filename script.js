dc687c4924914d814f9fa38bcf0b6bcb16ee30f5
bhavani <127599832+bhavanilakshmi23@users.noreply.github.com>
2023-08-06 12:10:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
 var div=document.createElement("div");
 div.style.textAlign="center";
 div.style.marginTop="100px";
+div.style.background="blueviolet";
 
 var input=document.createElement("input");
 input.setAttribute("type","text");
@@ -12,21 +12,27 @@ var button=document.createElement("button");
 button.setAttribute("type","button");
 button.innerHTML="Click me";
 button.style.margin="5px";
-button.style.background="green";
+button.style.background="skyblue";
 button.addEventListener("click",getdata);
 
-div.append(input,button);
+var active=document.createElement("div");
+active.style.fontSize="32px";
+active.style.fontFamily="Arial";
+active.style.color="grey";
+
+div.append(input,button,active);
 document.body.append(div);
 
 async function getdata(){
     let res=document.getElementById("countryname").value;
     console.log(res);
    try {
-    let res1=await fetch(`https://api.covid19api.com/dayone/country/${res}`);
+    let res1=await fetch(`https://api.covid19api.com/total/dayone/country/${res}`);
     let res2=await res1.json();
     console.log(res2);
     index=res2.length-1;
     console.log(` Active:${res2[index].Active}`);
+    active.innerHTML=`Active:${res2[index].Active}`;
    } catch (error) {
     console.log(error);
    }
